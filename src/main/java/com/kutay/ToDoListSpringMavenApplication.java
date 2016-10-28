fc2ca41ef517d083ccaa571bed565cf941ca52bf
kutayakyol <kutayakyolmkaol@gmail.com>
2016-10-28 16:28:37 +0300

    Initial commit

---
+++ src/main/java/com/kutay/ToDoListSpringMavenApplication.java
@@ -0,0 +1,12 @@
+package com.kutay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListSpringMavenApplication.class, args);
+	}
+}
